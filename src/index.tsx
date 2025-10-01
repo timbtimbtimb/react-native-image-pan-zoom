143d89fa45301b99b4fec3b2c97b3d2ef433886f
Timothée Billiet Cadart <timothee.billiet@arturia.com>
2025-10-01 20:39:34 +0200

    fix: fixed exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ViewPanZoom, { type ViewPanZoomRef } from './ViewPanZoom';
-import ViewPanCarousel, { type ViewPanCarouselRef } from './ViewPanCarousel';
+import ViewPanCarousel from './ViewPanCarousel';
 
 export default ViewPanZoom;
 
 export { ViewPanZoom, ViewPanCarousel };
 
-export type { ViewPanCarouselRef, ViewPanZoomRef };
+export type { ViewPanZoomRef };
